76aa7370d404da4ec9baa5d1728fc3178b4340dd
polterguy <polterguy@3126b1b2-c4f4-11de-8745-9f8d1d7459b8>
2009-12-04 05:08:55 +0000

    Added Google Analytics Controller

---
+++ trunk/Controllers/GoogleAnalyticsController/GoogleAnalyticsController.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Ra-Brix - A Modular-based Framework for building 
+ * Web Applications Copyright 2009 - Thomas Hansen 
+ * thomas@ra-ajax.org. Unless permission is 
+ * explicitly given this code is licensed under the 
+ * GNU Affero GPL version 3 which can be found in the 
+ * license.txt file on disc.
+ * 
+ */
+
+using System.Web;
+using System.Web.UI;
+using Ra.Brix.Loader;
+using SettingsRecords;
+
+namespace GoogleAnalyticsController
+{
+    [ActiveController]
+    public class GoogleAnalyticsController
+    {
+        [ActiveEvent(Name = "Page_Init")]
+        protected static void Page_Init(object sender, ActiveEventArgs e)
+        {
+            Page page = HttpContext.Current.Handler as Page;
+            if (page != null)
+            {
+                if(!page.IsPostBack)
+                {
+                    // Inject the Google Analytics tracking code...
+                    string trackingID = Settings.Instance["GoogleAnalyticsTrackingID"];
+                    if(!string.IsNullOrEmpty(trackingID))
+                    {
+                        string trackingCode = string.Format(@"
+<script type=""text/javascript"">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', '{0}']);
+_gaq.push(['_trackPageview']);
+(function() {{
+var ga = document.createElement('script');
+ga.src = ('https:' == document.location.protocol ?
+    'https://ssl' : 'http://www') +
+    '.google-analytics.com/ga.js';
+ga.setAttribute('async', 'true');
+document.documentElement.firstChild.appendChild(ga);
+}})();
+</script>", trackingID);
+                        LiteralControl ctrl = new LiteralControl();
+                        ctrl.Text = trackingCode;
+                        page.Form.Controls.Add(ctrl);
+                    }
+                }
+            }
+        }
+    }
+}
