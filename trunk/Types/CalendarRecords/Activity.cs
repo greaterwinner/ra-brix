d13198493d5e36f3107867c87b00eac4602ab6bd
polterguy <polterguy@3126b1b2-c4f4-11de-8745-9f8d1d7459b8>
2009-12-18 19:55:46 +0000

    Repetition on Calendar

--- trunk/Types/CalendarRecords/Activity.cs
+++ trunk/Types/CalendarRecords/Activity.cs
@@ -15,8 +15,36 @@ using UserRecords;
 namespace CalendarRecords
 {
     [ActiveType]
-    public class Activity : ActiveType<Activity>
+    public class Activity : ActiveType<Activity>, ICloneable
     {
+        public enum RepetitionPattern
+        {
+            None,
+            Weekly,
+            Monthly,
+            Yearly
+        };
+
+        public Activity()
+        {
+            Repetition = RepetitionPattern.None;
+        }
+
+        public RepetitionPattern Repetition
+        {
+            get
+            {
+                return (RepetitionPattern)Enum.Parse(typeof(RepetitionPattern), RepPatterImpl);
+            }
+            set
+            {
+                RepPatterImpl = value.ToString();
+            }
+        }
+
+        [ActiveField]
+        public string RepPatterImpl { get; set; }
+
         [ActiveField(IsOwner = false)]
         public User Creator { get; set; }
 
@@ -32,16 +60,97 @@ namespace CalendarRecords
         [ActiveField]
         public DateTime End { get; set; }
 
+        [ActiveField]
+        private string BundleID { get; set; }
+
+        private void SaveImpl()
+        {
+            base.Save();
+        }
+
         public override void Save()
         {
+            bool hasError = false;
             if (Start >= End)
             {
                 End = Start.AddMinutes(1);
-                base.Save(); 
+                hasError = true;
+            }
+            if (ID == 0)
+            {
+                // Initial save, need to create a BundleID for repetition pattern...
+                BundleID = Guid.NewGuid().ToString();
+            }
+            else
+            {
+                // Need to delete previous BundleID records...
+                foreach (Activity idx in Select(Criteria.Eq("BundleID", BundleID)))
+                {
+                    if (idx.ID != ID)
+                        idx.Delete();
+                }
+            }
+            if (Repetition == RepetitionPattern.Weekly)
+            {
+                SaveImpl();
+                DateTime idx = Start;
+                while (idx - Start < new TimeSpan(365, 0, 0, 0))
+                {
+                    idx = idx.AddDays(7);
+                    Activity n = ((ICloneable)this).Clone() as Activity;
+                    n.Start += (idx - Start);
+                    n.End += (idx - Start);
+                    n.SaveImpl();
+                }
+            }
+            if (Repetition == RepetitionPattern.Monthly)
+            {
+                SaveImpl();
+                DateTime idx = Start;
+                while (idx - Start < new TimeSpan(365, 0, 0, 0))
+                {
+                    idx = idx.AddMonths(1);
+                    Activity n = ((ICloneable)this).Clone() as Activity;
+                    n.Start += (idx - Start);
+                    n.End += (idx - Start);
+                    n.SaveImpl();
+                }
+            }
+            if (Repetition == RepetitionPattern.Yearly)
+            {
+                SaveImpl();
+                DateTime idx = Start;
+                while (idx - Start < new TimeSpan(3650, 0, 0, 0))
+                {
+                    idx = idx.AddYears(1);
+                    Activity n = ((ICloneable)this).Clone() as Activity;
+                    n.Start += (idx - Start);
+                    n.End += (idx - Start);
+                    n.SaveImpl();
+                }
+            }
+            else
+            {
+                SaveImpl();
+            }
+            if (hasError)
+            {
                 throw new ArgumentException(
                     "Cannot have the start date be equal or higher than the end date of the activity");
             }
-            base.Save();
+        }
+
+        object ICloneable.Clone()
+        {
+            Activity retVal = new Activity();
+            retVal.Body = Body;
+            retVal.BundleID = BundleID;
+            retVal.Creator = Creator;
+            retVal.End = End;
+            retVal.Header = Header;
+            retVal.Repetition = Repetition;
+            retVal.Start = Start;
+            return retVal;
         }
     }
 }
