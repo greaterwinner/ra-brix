85716bd63800dae5d4f0b8c8b1a85c9163e51413
polterguy <polterguy@3126b1b2-c4f4-11de-8745-9f8d1d7459b8>
2010-03-23 22:23:34 +0000

    ViewState serialized on server

---
+++ Portal/App_Code/RaBrixPageStatePersister.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Ra-Brix - A Modular-based Framework for building 
+ * Web Applications Copyright 2009 - Thomas Hansen 
+ * thomas@ra-ajax.org. Unless permission is 
+ * explicitly given this code is licensed under the 
+ * GNU Affero GPL version 3 which can be found in the 
+ * license.txt file on disc.
+ * 
+ */
+
+using System;
+using System.Web.UI;
+using System.IO;
+using Ra.Brix.Data;
+using System.Text;
+
+public class RaBrixPageStatePersister : PageStatePersister
+{
+    public RaBrixPageStatePersister(Page page)
+        : base(page)
+    { }
+
+    public override void Load()
+    {
+        IPersistViewState state = Ra.Brix.Data.Internal.Adapter.Instance as IPersistViewState;
+        LosFormatter formatter = new LosFormatter();
+        Pair pair = formatter.Deserialize(state.Load(Page.Session.SessionID, Page.Request.Url.ToString())) as Pair;
+        ViewState = pair.First;
+        ControlState = pair.Second;
+    }
+
+    public override void Save()
+    {
+        if (ViewState != null || ControlState != null)
+        {
+            if (Page.Session != null)
+            {
+                IPersistViewState state = Ra.Brix.Data.Internal.Adapter.Instance as IPersistViewState;
+                LosFormatter formatter = new LosFormatter();
+                StringBuilder builder = new StringBuilder();
+                using (StringWriter writer = new StringWriter(builder))
+                {
+                    formatter.Serialize(writer, new Pair(ViewState, ControlState));
+                }
+                state.Save(Page.Session.SessionID, Page.Request.Url.ToString(), builder.ToString());
+            }
+            else
+                throw new InvalidOperationException("Session needed for RaBrixPageStatePersister...");
+        }
+    }
+}
---
+++ Ra.Brix.Core/Ra.Brix.Data/IPersistViewState.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Ra-Brix - A Modular-based Framework for building 
+ * Web Applications Copyright 2009 - Thomas Hansen 
+ * thomas@ra-ajax.org. Unless permission is 
+ * explicitly given this code is licensed under the 
+ * GNU Affero GPL version 3 which can be found in the 
+ * license.txt file on disc.
+ * 
+ */
+
+namespace Ra.Brix.Data
+{
+    public interface IPersistViewState
+    {
+        void Save(string sessionId, string pageUrl, string content);
+        string Load(string sessionId, string pageUrl);
+    }
+}
