8fb53a2db3eae7ee0d13b58472a300177da2f2bb
polterguy <polterguy@3126b1b2-c4f4-11de-8745-9f8d1d7459b8>
2010-03-27 00:06:13 +0000

    Chrome Frame improvements...

---
+++ Controllers/DatedIEVersionController/DateIEController.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Ra-Brix - A Modular-based Framework for building 
+ * Web Applications Copyright 2009 - Thomas Hansen 
+ * thomas@ra-ajax.org. Unless permission is 
+ * explicitly given this code is licensed under the 
+ * GNU Affero GPL version 3 which can be found in the 
+ * license.txt file on disc.
+ * 
+ */
+
+using System;
+using Ra.Brix.Loader;
+using System.Web;
+using SettingsRecords;
+using System.Web.UI;
+
+namespace DatedIEVersionController
+{
+    [ActiveController]
+    public class DateIEController
+    {
+        [ActiveEvent(Name = "Page_Init_InitialLoading")]
+        protected void InitialLoading(object sender, ActiveEventArgs e)
+        {
+            InitializeChromeFrame();
+        }
+
+        private void InitializeChromeFrame()
+        {
+            if (HttpContext.Current.Request.Browser.Browser == "IE")
+            {
+                if (Settings.Instance.Get<bool>("UseChromeFrame", true))
+                {
+                    ActiveEvents.Instance.RaiseLoadControl(
+                        "DatedIEVersionModules.DatedIE",
+                        "dynTop");
+
+                    LiteralControl lit = new LiteralControl();
+                    lit.Text = @"
+<!--[if lt IE 8]>
+<meta http-equiv=""X-UA-Compatible"" content=""chrome=1"" />
+<![endif]-->";
+                    (HttpContext.Current.Handler as Page).Header.Controls.Add(lit);
+                }
+            }
+        }
+    }
+}
---
+++ Modules/DatedIEVersionModules/DatedIE.ascx
@@ -0,0 +1,28 @@
+﻿<%@ Assembly 
+    Name="DatedIEVersionModules" %>
+
+<%@ Control 
+    Language="C#" 
+    AutoEventWireup="true" 
+    Inherits="DatedIEVersionModules.DatedIE" %>
+
+<!--[if lt IE 8]>
+    <div style="padding:15px;position:fixed;top:5px;left:5px;width:275px;z-index:6000;background-color:White;color:Black;border:dashed 3px Red;">
+        <h1 style="color:Red;">WARNING!</h1>
+        <p>
+            Your browser is so old that it poses a security risk for your safety online!
+        </p>
+        <p>
+            You *REALLY* should update your browser to avoid getting infected by viruses and other
+            malware. Millions of people get their identities stolen every year due to bad IT security,
+            don't become the next victim!
+        </p>
+        <p>
+            This site might also not work optimally unless viewed with a more secure browser
+        </p>
+        <p>
+            Read more here: <b><a target="_new" href="http://ra-brix.org/update-internet-explorer">Update Internet Explorer</a></b>.
+        </p>
+    </div>
+<![endif]-->
+
